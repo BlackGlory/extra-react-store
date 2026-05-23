a6faf85cf034e5a45c8c08af08a8c49ead08d2a6
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2026-05-23 17:22:02 +0000

    refactor: extract the error message

--- src/create-store-context.ts
+++ src/create-store-context.ts
@@ -2,21 +2,17 @@ import { createContext } from 'react'
 import { IStore, StoreContext } from './types.js'
 
 export function createStoreContext<State>(): StoreContext<State> {
+  const message = 'StoreContext used outside of a Provider. Did you forget to wrap your component with <Context.Provider>?'
+
   const context = createContext<IStore<State>>({
     getState(): never {
-      throw new Error(
-        'StoreContext used outside of a Provider. Did you forget to wrap your component with <Context.Provider>?'
-      )
+      throw new Error(message)
     }
   , setState(): never {
-      throw new Error(
-        'StoreContext used outside of a Provider. Did you forget to wrap your component with <Context.Provider>?'
-      )
+      throw new Error(message)
     }
   , subscribe(): never {
-      throw new Error(
-        'StoreContext used outside of a Provider. Did you forget to wrap your component with <Context.Provider>?'
-      )
+      throw new Error(message)
     }
   })
 
