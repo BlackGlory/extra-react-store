51aa6c881c86d461ea50985bfc564ec370c6f18c
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2026-05-23 17:20:54 +0000

    feat(create-store-context): improve default stores

--- src/create-store-context.ts
+++ src/create-store-context.ts
@@ -2,6 +2,23 @@ import { createContext } from 'react'
 import { IStore, StoreContext } from './types.js'
 
 export function createStoreContext<State>(): StoreContext<State> {
-  const context = createContext<IStore<State>>({} as IStore<State>)
+  const context = createContext<IStore<State>>({
+    getState(): never {
+      throw new Error(
+        'StoreContext used outside of a Provider. Did you forget to wrap your component with <Context.Provider>?'
+      )
+    }
+  , setState(): never {
+      throw new Error(
+        'StoreContext used outside of a Provider. Did you forget to wrap your component with <Context.Provider>?'
+      )
+    }
+  , subscribe(): never {
+      throw new Error(
+        'StoreContext used outside of a Provider. Did you forget to wrap your component with <Context.Provider>?'
+      )
+    }
+  })
+
   return context
 }
